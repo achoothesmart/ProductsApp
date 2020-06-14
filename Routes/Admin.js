4c8e491c18c9b610b2b7e60e20ff2c6d81771197
Prasanna Ashok <v.prasannaashok@gmail.com>
2020-06-14 12:23:06 +0530

    Added .gitignore file & few changes

--- Routes/Admin.js
+++ Routes/Admin.js
@@ -3,15 +3,6 @@ const router = express.Router();
 const path = require('path');
 const app_path = require('../util/app-path');
 
-// const addProductPage = `
-// <html><head><title>Add Product</title></head><body>
-//     <div> <a href='/'>Back to Home page</a> </div>
-//     <form action='/product' method='POST'>
-//         <input type='text' name='product-name' placeholder='Product Name'/>
-//         <button type='submit'>Add Product</button>
-//     </form>
-// </body></html>`;
-
 router.get('/add-product',(req, res, next)=>{
     // res.send(addProductPage);
     res.sendFile(path.join(app_path.views, 'add-product.html'));
